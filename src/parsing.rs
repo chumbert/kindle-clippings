95ba58365e11a6e5ebb70da0f9fd9caed2d80e7c
chumbert <clement.humbert@gmail.com>
2024-07-28 13:44:47 +0200

    chore: refactor and test parsing (#9)

--- src/parsing.rs
+++ src/parsing.rs
@@ -90,7 +90,7 @@ lazy_static! {
     static ref ACTION_LINE_REGEX: Regex = Regex::new(
         r"^- Your (?P<action>\w+) (on page (?P<page>(\d+(-\d+)?)|(\w+))|at location (?P<location>\d+(-\d+)?)) (\| location (?<locationGroup>\d+(-\d+)?) )?\| Added on (?P<date>.+)$"
 ).unwrap();
-
+    
 }
 
 /// Parses clipping file content
@@ -123,32 +123,8 @@ pub fn parse_clippings(content: &str) -> Vec<Entry> {
             break;
         }
 
-        let title_author_line = lines.next().unwrap();
-        let caps = match TITLE_AUTHOR_REGEX.captures(&title_author_line) {
-            Some(author_title_match) => author_title_match,
-            None => Regex::new("(?P<title>.*)")
-                .unwrap()
-                .captures(&title_author_line)
-                .unwrap(),
-        };
-        let title = caps.name("title").unwrap().as_str().to_string();
-        let author = caps
-            .name("author")
-            .map_or_else(|| None, |x| Some(x.as_str().to_string()));
-
-        let action_line = lines.next().unwrap();
-        let caps = match ACTION_LINE_REGEX.captures(&action_line) {
-            Some(action_match) => action_match,
-            None => panic!("Can't parse action line: {}", action_line),
-        };
-        let action = caps["action"].to_string();
-        let page = caps
-            .name("page")
-            .map_or_else(|| None, |x| Some(x.as_str().to_string()));
-        let location = caps
-            .name("locationGroup")
-            .map_or_else(|| None, |x| Some(x.as_str().to_string()));
-        let date = caps["date"].to_string();
+        let (title, author) = parse_title_author(&lines.next().unwrap());
+        let (action, page, location, date) = parse_action_line(&lines.next().unwrap());
 
         let mut content = None;
         if action == "Highlight" || action == "Note" {
@@ -159,7 +135,7 @@ pub fn parse_clippings(content: &str) -> Vec<Entry> {
                 }
                 quote_lines.push(lines.next().unwrap());
             }
-            content = Some(quote_lines.join(" "));
+            content = Some(quote_lines.join(" ").trim().to_string());
         }
 
         if action == "Bookmark" {
@@ -183,4 +159,125 @@ pub fn parse_clippings(content: &str) -> Vec<Entry> {
     }
 
     entries
+}
+
+fn parse_title_author(line: &str) -> (String, Option<String>) {
+    let caps = match TITLE_AUTHOR_REGEX.captures(line) {
+        Some(author_title_match) => author_title_match,
+        None => Regex::new("(?P<title>.*)")
+            .unwrap()
+            .captures(line)
+            .unwrap(),
+    };
+    let title = caps.name("title").unwrap().as_str().to_string();
+    let author = caps
+        .name("author")
+        .map_or_else(|| None, |x| Some(x.as_str().to_string()));
+
+    (title, author)
+}
+
+fn parse_action_line(line: &str) -> (String, Option<String>, Option<String>, String) {
+    let caps = match ACTION_LINE_REGEX.captures(line) {
+        Some(action_match) => action_match,
+        None => panic!("Can't parse action line: {}", line),
+    };
+    let action = caps["action"].to_string();
+    let page = caps
+        .name("page").map(|x| x.as_str().to_string());
+    
+    let only_location = caps.name("location").map(|x| x.as_str().to_string());
+
+    let location = caps
+        .name("locationGroup").map(|x| x.as_str().to_string());
+    
+    let date = caps["date"].to_string();
+
+    (action, page, only_location.or(location), date)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn parse_and_get_first(content: &str) -> Entry {
+        let parsed = parse_clippings(content);
+        assert_eq!(1, parsed.len());
+        parsed.into_iter().next().unwrap()
+    }
+
+    #[test]
+    fn test_parse_entry_without_author() {
+        let content = "The Grapes of Wrath \n- Your Highlight on page 12453252 | Added on Friday, 5 July 2024 09:55:52\n\nSmall for convenience.\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!(None, entry.author());
+        assert_eq!("The Grapes of Wrath".to_string(), entry.title());
+    }
+
+    #[test]
+    fn test_parse_action_line_only_location() {
+        let content = "The Grapes of Wrath \n- Your Highlight at location 3867-3869 | Added on Friday, 5 July 2024 09:55:52\n\nSmall for convenience.\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!(None, entry.page());
+        assert_eq!(Some("3867-3869".to_string()), entry.location());
+    }
+
+    #[test]
+    fn test_parse_action_line_page_and_location() {
+        let content = "The Grapes of Wrath \n- Your Highlight on page 14 | location 214-215 | Added on Friday, 5 July 2024 09:55:52\n\nSmall for convenience.\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!(Some("14".to_string()), entry.page());
+        assert_eq!(Some("214-215".to_string()), entry.location());
+    }
+
+    #[test]
+    fn test_parse_action_line_only_page() {
+        let content = "The Grapes of Wrath \n- Your Highlight on page 14 | Added on Friday, 5 July 2024 09:55:52\n\nSmall for convenience.\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!(Some("14".to_string()), entry.page());
+        assert_eq!(None, entry.location());
+    }
+
+    #[test]
+    fn test_parse_action_line_highlight() {
+        let content = "The Grapes of Wrath \n- Your Highlight on page 14 | Added on Friday, 5 July 2024 09:55:52\n\nSmall for convenience.\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!("Highlight".to_string(), entry.action());
+    }
+
+    #[test]
+    fn test_parse_action_line_bookmark() {
+        let content = "The Grapes of Wrath \n- Your Bookmark on page 14 | Added on Friday, 5 July 2024 09:55:52\n\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!("Bookmark".to_string(), entry.action());
+    }
+
+    #[test]
+    fn test_parse_action_line_note() {
+        let content = "The Grapes of Wrath \n- Your Note on page 14 | Added on Friday, 5 July 2024 09:55:52\n\nSmall for convenience.\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!("Note".to_string(), entry.action());
+    }
+
+    #[test]
+    fn test_parse_content_highlight() {
+        let content = "The Grapes of Wrath \n- Your Highlight on page 12453252 | Added on Friday, 5 July 2024 09:55:52\n\nSmall for convenience.\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!(Some("Small for convenience.".to_string()), entry.content());
+    }
+
+    #[test]
+    fn test_parse_content_note() {
+        let content = "The Grapes of Wrath \n- Your Highlight on page 12453252 | Added on Friday, 5 July 2024 09:55:52\n\nSmall for convenience.\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!(Some("Small for convenience.".to_string()), entry.content());
+    }
+
+    #[test]
+    fn test_parse_content_bookmark() {
+        let content = "The Grapes of Wrath \n- Your Bookmark on page 12453252 | Added on Friday, 5 July 2024 09:55:52\n\n";
+        let entry = parse_and_get_first(content);
+        assert_eq!(None, entry.content());
+    }
 }
